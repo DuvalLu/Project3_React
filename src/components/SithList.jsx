60d4672e409e673aae6e71b18b3203478082a30b
Luis Velasquez <velasquez.lu@outlook.com>
2026-04-10 10:51:47 -0400

    update sithlist

--- src/components/SithList.jsx
+++ src/components/SithList.jsx
@@ -1,6 +1,16 @@
 import SithCard from "./SithCard";
 
 function SithList({ sith, vanquishSith, deleteSith }) {
+  const allVanquished = sith.every((oneSith) => oneSith.vanquished === true);
+
+  if (allVanquished) {
+    return (
+      <div>
+        <h2>Peace has been restored to the galaxy!</h2>
+      </div>
+    );
+  }
+
   return (
     <div>
       {sith.map((oneSith) => (
